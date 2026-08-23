c056a7bff316db3bc6f7870be92bcc9001518846
Aryan Yadav <contactaryanhq@gmail.com>
2026-08-23 12:54:18 +0530

    added more files

--- C Tutorials/chapter9-structure/3students-data.c
+++ C Tutorials/chapter9-structure/3students-data.c
@@ -2,11 +2,11 @@
 #include <string.h>
 
 int main() {
-    struct student{
+    typedef struct student{
         char name[100];
         int roll;
         float cgpa;
-    };
+    } stu;
 
     struct student s1;
     s1.roll = 01;
@@ -16,14 +16,9 @@ int main() {
     printf("Name : %s\n Roll No.: %d\n CGPA : %.2f\n", s1.name, s1.roll, s1.cgpa);
 
     struct student s2 = {"Rohan", 02, 6.7};
-
     printf("Name : %s\n Roll No.: %d\n CGPA : %.2f\n", s2.name, s2.roll, s2.cgpa);
 
-    struct student s3;
-    s3.roll = 03;
-    s3.cgpa = 2.1;
-    strcpy(s3.name, "Tom");
-
+    stu s3 = {"Tom", 03, 2.1};
     printf("Name : %s\n Roll No.: %d\n CGPA : %.2f\n", s3.name, s3.roll, s3.cgpa);
 
     return 0;
